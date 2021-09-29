bcede493f27e2ab69e4faa85536050438436a0de
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-09-29 19:32:28 -0300

    criação da model.entity.Lancamento e enums model.enums.StatusLancamento e model.enums.TipoLancamento

---
+++ minhas-financas-api/src/main/java/com/carreiras/github/minhasfinancasapi/model/enums/StatusLancamento.java
@@ -0,0 +1,8 @@
+package com.carreiras.github.minhasfinancasapi.model.enums;
+
+public enum StatusLancamento {
+
+    PENDENTE,
+    CANCELADO,
+    EFETIVADO
+}
---
+++ minhas-financas-api/src/main/java/com/carreiras/github/minhasfinancasapi/model/enums/TipoLancamento.java
@@ -0,0 +1,7 @@
+package com.carreiras.github.minhasfinancasapi.model.enums;
+
+public enum TipoLancamento {
+
+    RECEITA,
+    DESPESA
+}
