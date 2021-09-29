b33ffaa4a6fce23a4bb6a7c92aa0ec5f6c6bee7a
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-09-29 18:49:39 -0300

    criação do service.impl.UsuarioServiceImpl

---
+++ minhas-financas-api/src/main/java/com/carreiras/github/minhasfinancasapi/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,9 @@
+package com.carreiras.github.minhasfinancasapi.service.impl;
+
+import com.carreiras.github.minhasfinancasapi.service.UsuarioService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+}
