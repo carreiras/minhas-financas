f0b16789e7d146de0b00d1736f3d64ed44ea0a83
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-09-29 19:39:32 -0300

    criação do service.LancamentoServiceImpl

---
+++ minhas-financas-api/src/main/java/com/carreiras/github/minhasfinancasapi/service/impl/LancamentoServiceImpl.java
@@ -0,0 +1,8 @@
+package com.carreiras.github.minhasfinancasapi.service.impl;
+
+import com.carreiras.github.minhasfinancasapi.service.LancamentoService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LancamentoServiceImpl implements LancamentoService {
+}
