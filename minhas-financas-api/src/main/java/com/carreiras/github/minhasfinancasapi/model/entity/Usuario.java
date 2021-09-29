2b5dcc386ef15589698deccdc021de60aef4d65a
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-09-29 18:40:35 -0300

    criação da model.entity.Usuario

---
+++ minhas-financas-api/src/main/java/com/carreiras/github/minhasfinancasapi/model/entity/Usuario.java
@@ -0,0 +1,31 @@
+package com.carreiras.github.minhasfinancasapi.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "usuario", schema = "financas")
+public class Usuario {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "senha")
+    private String senha;
+}
