58ec121f8f14aa026d02982d0a690ac2982fe7ff
Ewerton Carreira <ewertoncarreira@gmail.com>
2021-09-29 19:36:16 -0300

    criação do model.repository.LancamentoRepository

--- minhas-financas-api/src/main/java/com/carreiras/github/minhasfinancasapi/model/repository/LancamentoRepository.java
+++ minhas-financas-api/src/main/java/com/carreiras/github/minhasfinancasapi/model/repository/LancamentoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface LancamentoRepository extends JpaRepository<Usuario, Long> {
+public interface LancamentoRepository extends JpaRepository<LancamentoRepository, Long> {
 }
